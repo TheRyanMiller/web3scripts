df0a89f5c8b5cc062666e27c22590e9ef84f661e
Ryan Miller <rmiller07@gmail.com>
2021-01-02 17:55:25 -0500

    added cron for withdraw

---
+++ cron.js
@@ -0,0 +1,10 @@
+const cron = require('node-cron');
+const moment = require('moment');
+const check_vault_withdrawable = require('./check_vault_withdrawable.js');
+require('dotenv').config();
+
+
+let check_if_withdrawable = cron.schedule("* * * * *", () => {
+    console.log("---"+new Date()+"---");
+    check_vault_withdrawable("crvSBTC");
+})
