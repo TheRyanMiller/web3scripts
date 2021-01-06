4e61e8751fc0ae51cdd21b4a3bf3cc5f05382289
Ryan Miller <rmiller07@gmail.com>
2021-01-06 00:47:13 -0500

    added sort

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web3 Scripts
+
+Scripts to query data from Ethereum smart contracts. Mainly Yearn vaults.
--- get_harvest_status_of_crv_vaults.js
+++ get_harvest_status_of_crv_vaults.js
@@ -19,19 +19,19 @@ let deployBlock = 11191799;
 
 contract.getPastEvents("StrategyAdded",{fromBlock: deployBlock, toBlock: 'latest'}).then((events,error)=>{
     let calculateHarvestPromises = [];
-    let requriedHarvestPromises = [];
+    let requiredHarvestPromises = [];
     events.forEach(e=>{
-        vault.strategyAddress = e.returnValues['0'];
-        vault.requiredHarvest = e.returnValues['1'];
         vaultData.forEach(v => {
-            if(v.strategyAddress === vault.strategyAddress){
+            if(v.strategyAddress === e.returnValues['0']){
                 vault.vaultAlias = v.vaultAlias;
                 vault.vaultAddress = v.address;
             }
         });
+        vault.strategyAddress = e.returnValues['0'];
+        vault.requiredHarvest = e.returnValues['1'];
         harvestableCrvVaults.push(vault);
         calculateHarvestPromises.push(contract.methods.calculateHarvest(vault.strategyAddress).call());
-        requriedHarvestPromises.push(contract.methods.requiredHarvest(vault.strategyAddress).call());
+        requiredHarvestPromises.push(contract.methods.requiredHarvest(vault.strategyAddress).call());
         vault = {};
     })
 
@@ -39,7 +39,7 @@ contract.getPastEvents("StrategyAdded",{fromBlock: deployBlock, toBlock: 'latest
         for(let i=0;i<values.length;i++){
             harvestableCrvVaults[i].currentHarvestableCrv = values[i] / 1e18;
         }
-        Promise.all(requriedHarvestPromises).then(values=>{
+        Promise.all(requiredHarvestPromises).then(values=>{
             for(let i=0;i<values.length;i++){
                 harvestableCrvVaults[i].requiredHarvest = values[i]  / 1e18;
                 harvestableCrvVaults[i].remainingUntilHarvest = 
@@ -47,9 +47,20 @@ contract.getPastEvents("StrategyAdded",{fromBlock: deployBlock, toBlock: 'latest
                     harvestableCrvVaults[i].requiredHarvest - harvestableCrvVaults[i].currentHarvestableCrv : 0;
                 harvestableCrvVaults[i].workable = harvestableCrvVaults[i].remainingUntilHarvest === 0 && harvestableCrvVaults[i].requiredHarvest > 0;
             }
+            harvestableCrvVaults.sort(sortByProperty("remainingUntilHarvest")) // Sort remaining until harvest towards the top
             console.log(harvestableCrvVaults);
         })
     })
 })
+
+function sortByProperty(property){  
+    return function(a,b){  
+       if(a[property] > b[property])  
+          return 1;  
+       else if(a[property] < b[property])  
+          return -1;  
+       return 0;  
+    }  
+ }
 
 
